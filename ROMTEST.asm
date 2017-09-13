4a99710e5946731a6d75d85a8f756bf3124a3b93
unknown <mgnjulien@gmail.com>
2017-09-13 19:01:44 +0200

    test the OAM, work on link between sprites and pallettes

--- ROMTEST.asm
+++ ROMTEST.asm
@@ -1,5 +1,5 @@
-.include "soft\header.asm"
-.include "soft\snes.asm"
+.include "SNES-ROMTEST\header.asm"
+.include "SNES-ROMTEST\snes.asm"
 
 Main:
 
@@ -15,7 +15,9 @@ Main:
 	SNES_BG1SC $10
 	SNES_BGNBA $00 $00
 	
-	SNES_TM $01
+	SNES_OBJSEL $A3
+	
+	SNES_TM $11
 	
 	SNES_VMAINC $80
 	
@@ -33,12 +35,23 @@ Main:
 	
 	SNES_MDMAEN $03
 	
+	SNES_CGADD $80
+	SNES_DMA0_ADD pallette_perso $0020
+	SNES_MDMAEN $01
+	
+	
 	SNES_DMA0 $01
 	SNES_DMA0_BADD $18
 	
 	SNES_VMADD $0000
 	
 	SNES_DMA0_ADD Data $0020
+	
+	SNES_MDMAEN $01
+	
+	SNES_VMADD $6000
+	
+	SNES_DMA0_ADD Sprite $0800
 	
 	SNES_MDMAEN $01
 	
@@ -53,13 +66,69 @@ Main:
 	
 	SNES_NMITIMEN $81
 	
+	;x sprite 1
+    lda #0 
+    sta 0 
+    
+    ;y sprite 1
+    lda #100 
+    sta 1
+	
 	
 	Game:
 		wai
 		
+		lda STDCONTROL1H
+		and #$01
+		cmp #$01
+		bne +
+		
+			inc 0
+			
+		+:
+		lda STDCONTROL1H
+		and #$02
+		cmp #$02
+		bne +
+		
+			dec 0
+			
+		+:
+		lda STDCONTROL1H
+		and #$04
+		cmp #$04
+		bne +
+		
+			inc 1
+		
+		+:
+		lda STDCONTROL1H
+		and #$08
+		cmp #$08
+		bne +
+		
+			dec 1
+			
+		+:
+		
 	jmp Game
 	
 VBlank:
+	;on balance l'oam ici, tous ce qui doit etre mis à jour en temps réel à l'écran en fait
+	lda #00
+	sta OAMADDL
+	
+	lda 0
+	sta OAMDATA
+	
+	lda 1
+	sta OAMDATA
+	
+	lda #0
+	sta OAMDATA
+	
+	lda #$20
+	sta OAMDATA
 	
 	rti
 		
@@ -83,3 +152,25 @@ Data:
 Tileset: 
     .dw $0000 , $0001 , $0001 , $0001 , $0001 , $0001  , $0001 , $0001  
     .dw $0401 , $0401 , $0401 , $0401 , $0401 , $0401  , $0401 , $0401		
+	
+Sprite:
+    .db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00
+    .db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00
+    .db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00
+    .db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00
+    .db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00
+    .db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $0c, $00, $1e, $00, $1a, $00
+    .db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $0c, $00, $1e, $00, $1a, $00
+    .db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00
+    .db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00
+    .db $1a, $00, $0c, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $ff, $ff
+    .db $1a, $00, $0c, $00, $00, $00, $00, $01, $00, $01, $00, $01, $00, $00, $ff, $ff
+    .db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $80, $80
+    .db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00
+    .db $80, $80, $ff, $ff, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00
+    .db $00, $00, $ff, $ff, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00
+    .db $80, $80, $80, $80, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00
+
+
+pallette_perso:
+    .dw $3c05, $ff7f, $5d05, $1d04
